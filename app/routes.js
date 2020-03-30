0ec8baac2fb190733718e4cbbaa90a219284e2f5
Michael Gearon <mgearon@companieshouse.gov.uk>
2020-03-30 08:08:20 +0100

    Functionality for error messages

--- app/routes.js
+++ app/routes.js
@@ -2,5 +2,30 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+router.get('/', function (req, res) {
+  req.session.destroy()
+  res.render('index', {
+  })
+})
 
+router.get('/obliged-entity-type', function (req, res) {
+  res.render('obliged-entity-type', {
+  })
+})
+
+router.post('/obliged-entity-type', function (req, res) {
+  var errors = []
+  if (typeof req.session.data['obliged-type'] === 'undefined') {
+    errors.push({
+      text: 'Select what type of obliged entity you are',
+      href: '#obliged-type'
+    })
+    res.render('obliged-entity-type', {
+      errorType: true,
+      errorList: errors
+    })
+  } else {
+    res.redirect('obliged-entity-details-organisation')
+  }
+})
 module.exports = router
