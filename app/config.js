53aaf5ff5d2211c2aeb218ead86df29841a4a86f
Emma Lynham <elynham@companieshouse.gov.uk>
2020-04-03 07:30:04 +0100

    Change to service name

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Emergency filing service',
+  serviceName: 'Upload a document to Companies House',
 
   // Default port that prototype runs on
   port: '3000',
