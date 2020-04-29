e1d79c3d4696e11a6f8c4028d9ca6803563f46b8
Michael Gearon <mgearon@companieshouse.gov.uk>
2020-04-29 10:37:27 +0100

    Adding validation for character length

--- app/routes.js
+++ app/routes.js
@@ -76,9 +76,21 @@ router.get('/company-number', function (req, res) {
 
 router.post('/company-number', function (req, res) {
   var errors = []
+  var str = req.session.data['company-number']
+  var n = str.length
+
   if (req.session.data['company-number'] === '') {
     errors.push({
       text: 'Enter the company number',
+      href: '#company-number'
+    })
+    res.render('company-number', {
+      errorNum: true,
+      errorList: errors
+    })
+  } else if (n !== 8) {
+    errors.push({
+      text: 'Company number must be 8 characters ',
       href: '#company-number'
     })
     res.render('company-number', {
